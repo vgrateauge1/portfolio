0c270aadf7ca269b540087a3eb777c8e3a160462
slydragonn <alogo18m@gmail.com>
2025-01-16 23:28:49 -0500

    fix: navbar position

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -27,7 +27,7 @@ const socials = [
 
 const Navbar = () => {
   return (
-    <nav className="z-50 fixed top-5 left-0 right-0 my-0 mx-auto max-w-max px-5 py-2 border border-slate-200 dark:border-slate-700 rounded-full bg-slate-50/30 dark:bg-slate-900/30 backdrop-blur-md">
+    <nav className="z-40 fixed top-5 left-0 right-0 my-0 mx-auto max-w-max px-5 py-2 border border-slate-200 dark:border-slate-700 rounded-full bg-slate-50/30 dark:bg-slate-900/30 backdrop-blur-md">
       <ul className="flex items-center justify-center gap-8 text-slate-700 dark:text-slate-300 text-xl">
         {socials.map((social, index) => (
           <li key={index} className="hover:scale-90 duration-150">
